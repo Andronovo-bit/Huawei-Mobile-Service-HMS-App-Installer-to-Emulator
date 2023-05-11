5ea6f3e6eb10b3a7608c515c8adc12c1502ab429
Seyyid Yiğit <seyyid364@gmail.com>
2023-05-11 14:22:33 +0300

    Init MAUI Project for HMS Installer

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace HuaweiHMSInstaller;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HuaweiHMSInstaller;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
