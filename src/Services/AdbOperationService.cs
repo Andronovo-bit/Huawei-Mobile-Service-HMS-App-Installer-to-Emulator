677624b2e549ad9beee13987292e68838ea4cca6
Seyyid Yiğit <seyyid364@gmail.com>
2023-05-25 15:45:00 +0300

    Add Options for using Dependecy Injection and created DownloadApk from internet function. -Fix CreateDirectory before download adb if not exist.

--- src/Services/AdbOperationService.cs
+++ src/Services/AdbOperationService.cs
@@ -5,7 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AdvancedSharpAdbClient;
+using AdvancedSharpAdbClient.DeviceCommands;
 using HuaweiHMSInstaller.Helper;
+using HuaweiHMSInstaller.Models;
+using Microsoft.Extensions.Options;
+using static AdvancedSharpAdbClient.DeviceCommands.PackageManager;
 
 namespace HuaweiHMSInstaller.Services
 {
@@ -18,41 +22,42 @@ namespace HuaweiHMSInstaller.Services
         // Use HttpClientFactory or dependency injection to create HttpClient instances
         private readonly HttpClient _client;
         private readonly AdbClient _adbClient;
+        private readonly GlobalOptions _options;
+        
 
-        public IProgress<float> Progress { get; set; }
 
-
-        public AdbOperationService(HttpClient client)
+        public AdbOperationService(HttpClient client, IOptions<GlobalOptions> options)
         {
             _adbClient = new AdbClient();
+            _options = options.Value;
             _client = client ?? throw new ArgumentNullException(nameof(client));
 
         }
-
         public bool CheckAdbServer()
         {
             return AdbServer.Instance.GetStatus().IsRunning;
         }
-
         // A public method that downloads and extracts the adb file
         public async Task DownloadAdbFromInternetAsync(IProgress<float> progress = null)
         {
             // Get the file name from the URL
             string fileName = Path.GetFileName(AdbUrl);
+
+            //if folder not exist create folder
+            Directory.CreateDirectory(_options.ProjectOperationPath);
+
             // Create a file stream to store the downloaded data
-            using (var file = new FileStream(Path.Combine(Path.GetTempPath(), fileName), FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+            using (var file = new FileStream(Path.Combine(_options.ProjectOperationPath, fileName), FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
             {
                 // Download the file using the custom extension method
-                await _client.DownloadAsync(AdbUrl, file, Progress);
+                await _client.DownloadAsync(AdbUrl, file, progress);
             }
 
             // Extract the file to the destination folder
-            ZipFile.ExtractToDirectory(Path.Combine(Path.GetTempPath(), fileName), Path.Combine(Path.GetTempPath(), AdbFolder),true);
+            ZipFile.ExtractToDirectory(Path.Combine(_options.ProjectOperationPath, fileName), Path.Combine(_options.ProjectOperationPath, AdbFolder),true);
             // Delete the file
-            File.Delete(Path.Combine(Path.GetTempPath(), fileName));
+            File.Delete(Path.Combine(_options.ProjectOperationPath, fileName));
         }
-
-
         public async Task<List<DeviceData>> GetDevices()
         {
             var devices = await _adbClient.GetDevicesAsync();
@@ -62,5 +67,16 @@ namespace HuaweiHMSInstaller.Services
             }
             return devices;
         }
+        
+        public async Task DownloadApkFromInternetAsync(string apkUrl, string apkName, IProgress<float> progress = null)
+        {
+            // Create a file stream to store the downloaded data
+            using (var file = new FileStream(Path.Combine(_options.ProjectOperationPath, apkName), FileMode.Create, FileAccess.Write, FileShare.None, 8192, true))
+            {
+                // Download the file using the custom extension method
+                await _client.DownloadAsync(apkUrl, file, progress);
+            }
+        }
+
     }
 }
--- src/Services/IAdbOperationService.cs
+++ src/Services/IAdbOperationService.cs
@@ -4,14 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AdvancedSharpAdbClient.DeviceCommands.PackageManager;
 
 namespace HuaweiHMSInstaller.Services
 {
     public interface IAdbOperationService
     {
-        public IProgress<float> Progress { get;set; }
         public bool CheckAdbServer();
         public Task DownloadAdbFromInternetAsync(IProgress<float> progress = null);
         public Task<List<DeviceData>> GetDevices();
+        public Task DownloadApkFromInternetAsync(string apkUrl, string apkName, IProgress<float> progress = null);
     }
 }
