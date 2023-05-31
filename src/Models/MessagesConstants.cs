7376cce7ff231708f0622cad5e111d5da5d3a95c
Seyyid Yiğit <seyyid364@gmail.com>
2023-05-31 09:17:32 +0300

    Edited AdbMessagesConst message use by Localization.

--- src/Models/MessagesConstants.cs
+++ src/Models/MessagesConstants.cs
@@ -1,31 +1,57 @@
-﻿namespace HuaweiHMSInstaller.Models
+﻿using HuaweiHMSInstaller.Services;
+using LocalizationResourceManager.Maui;
+
+namespace HuaweiHMSInstaller.Models
 {
     public static class AdbMessagesConst
     {
-        public const string DownloadingADBDriver = "Downloading the ADB Driver";
-        public const string InstallingADBDriver = "Installing the ADB Driver";
-        public const string DownloadingHMSCore = "Downloading the HMS Core";
-        public const string InstallingHMSCore = "Installing HMS Core";
-        public const string DownloadingHMSAppGallery = "Downloading the HMS AppGallery";
-        public const string InstallingHMSAppGallery = "Installing the HMS AppGallery";
-        public const string DownloadingGame = "Downloading Game";
-        public const string InstallingGame = "Installing Game";
-        public const string Finishing = "Finishing";
+        private static ILocalizationResourceManager _localizationResourceManager { get => Services.ServiceProvider.GetService<ILocalizationResourceManager>(); }
 
-        public readonly static Dictionary<string,bool> Messages = 
-        new Dictionary<string, bool>
+        // Use constants instead of properties for the message keys
+        private const string DownloadingADBDriverKey = "downloading_adb_driver";
+        private const string InstallingADBDriverKey = "installing_adb_driver";
+        private const string DownloadingHMSCoreKey = "downloading_hms_core";
+        private const string InstallingHMSCoreKey = "installing_hms_core";
+        private const string DownloadingHMSAppGalleryKey = "downloading_appgallery";
+        private const string InstallingHMSAppGalleryKey = "installing_appgallery";
+        private const string DownloadingGameKey = "downloading_game";
+        private const string InstallingGameKey = "installing_game";
+        private const string FinishingKey = "finishing";
+
+        // Use a helper method to get the localized value for a given key
+        private static string GetLocalizedValue(string key) => _localizationResourceManager.GetValue(key);
+
+        // Use properties to get the localized messages
+        public static string DownloadingADBDriver => GetLocalizedValue(DownloadingADBDriverKey);
+        public static string InstallingADBDriver => GetLocalizedValue(InstallingADBDriverKey);
+        public static string DownloadingHMSCore => GetLocalizedValue(DownloadingHMSCoreKey);
+        public static string InstallingHMSCore => GetLocalizedValue(InstallingHMSCoreKey);
+        public static string DownloadingHMSAppGallery => GetLocalizedValue(DownloadingHMSAppGalleryKey);
+        public static string InstallingHMSAppGallery => GetLocalizedValue(InstallingHMSAppGalleryKey);
+        public static string DownloadingGame => GetLocalizedValue(DownloadingGameKey);
+        public static string InstallingGame => GetLocalizedValue(InstallingGameKey);
+        public static string Finishing => GetLocalizedValue(FinishingKey);
+
+        // Use a helper method to initialize the dictionary with the message keys and values
+        private static Dictionary<string, bool> InitializeMessages()
         {
-            { DownloadingADBDriver, true },
-            { InstallingADBDriver, true },
-            { DownloadingHMSCore, true },
-            { DownloadingHMSAppGallery, true },
-            { DownloadingGame, true },
-            { InstallingHMSCore, true },
-            { InstallingHMSAppGallery, true },
-            { InstallingGame, true },
-            { Finishing, true },
-        };
+            var messages = new Dictionary<string, bool>
+            {
+                { DownloadingADBDriver, true },
+                { InstallingADBDriver, true },
+                { DownloadingHMSCore, true },
+                { InstallingHMSCore, true },
+                { DownloadingHMSAppGallery, true },
+                { InstallingHMSAppGallery, true },
+                { DownloadingGame, true },
+                { InstallingGame, true },
+                { Finishing, true }
+            };
+            return messages;
+        }
 
+        // Use a readonly field instead of a property for the dictionary
+        public readonly static Dictionary<string, bool> Messages = InitializeMessages();
     }
 
     public static class HmsInfoMessagesConst{
